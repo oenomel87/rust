95e1e5b50ebfd039d218995a5a64162063d8a0ce
yspark <ysparknanum@gmail.com>
2018-08-25 18:48:06 +0900

    philosopher 예제

---
+++ projects/philosopher/src/main.rs
@@ -0,0 +1,60 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::{Mutex, Arc};
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+impl Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+        println!("{} 님이 식사를 시작합니다.", self.name);
+        thread::sleep(Duration::new(1, 0));
+        println!("{} 님이 식사를 마쳤습니다.", self.name);
+    }
+}
+
+fn main() {
+    let table = Arc::new(Table { forks: vec! [
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
+    let philosophers = vec![
+        Philosopher::new("Judith Butler", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4),
+    ];
+
+    let handlers: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handlers {
+        h.join().unwrap();
+    }
+}
